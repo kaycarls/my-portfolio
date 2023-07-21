330504e4c0bbe23c027cf8508f27ff2e4c7277f8
Syafiq <syafiq.awangku@hotmail.com>
2023-07-21 17:38:33 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-container fluid class="h-screen text-center pt-10">
+      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, eveniet.</p>
+      <v-btn
+        class="ma-2"
+        color="blue"
+      >
+        Accept
+        <v-icon
+          end
+          icon="mdi-checkbox-marked-circle"
+        ></v-icon>
+      </v-btn>
+    </v-container>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+export default defineNuxtConfig({
+  modules: [
+    '@invictus.codes/nuxt-vuetify',
+    '@nuxtjs/google-fonts',
+  ],
+  vuetify: {
+    /* vuetify options */
+    vuetifyOptions: {
+      // @TODO: list all vuetify options
+    },
+
+    moduleOptions: {
+      /* nuxt-vuetify module options */
+      treeshaking: true,
+      useIconCDN: true,
+
+      /* vite-plugin-vuetify options */
+      styles: true, // | 'none' | 'expose' | 'sass' | { configFile: string },
+      autoImport: true, // | false,
+      useVuetifyLabs: true, // | false, 
+    },
+  },
+})
+
